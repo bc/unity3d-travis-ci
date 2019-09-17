fb72e9b252a9baebbbd1337fdcd8535dcc1e7efc
Brian Cohn <brian.cohn@usc.edu>
2019-09-17 02:32:39 -0700

    Update install-unity.sh

--- travis-build/install-unity.sh
+++ travis-build/install-unity.sh
@@ -3,8 +3,8 @@
 # See https://unity3d.com/get-unity/download/archive
 # to get download URLs
 UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
-UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/46dda1414e51/MacEditorInstaller/Unity-2017.2.0f3.pkg"
-UNITY_WINDOWS_TARGET_PACKAGE_URL="https://beta.unity3d.com/download/46dda1414e51/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.2.0f3.pkg"
+UNITY_OSX_PACKAGE_URL="https://beta.unity3d.com/download/9dace1eed4cc/MacEditorTargetInstaller/UnitySetup-Mac-IL2CPP-Support-for-Editor-2019.2.5f1.pkg?_ga=2.92062803.812760864.1568676221-1144215279.1552505643"
+UNITY_WINDOWS_TARGET_PACKAGE_URL="https://beta.unity3d.com/download/9dace1eed4cc/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-2019.2.5f1.pkg?_ga=2.92062803.812760864.1568676221-1144215279.1552505643"
 
 
 # Downloads a file if it does not exist
